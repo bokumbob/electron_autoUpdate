13fedfff0f3b62fd57f19a6375c9432b15106cde
JSJEONG <jsjeong@clew.tech>
2023-04-17 14:14:14 +0900

    another example

--- main.js
+++ main.js
@@ -1,58 +1,94 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron')
-const { autoUpdater } = require('electron-updater')
-const path = require('path')
-let mainWindow;
+// const {app, BrowserWindow, Menu, ipcMain} = require('electron')
+// const { autoUpdater } = require('electron-updater')
+// const path = require('path')
+// let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+// function createWindow () {
+//   mainWindow = new BrowserWindow({
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
 
-  mainWindow.loadFile('index.html')
+//   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-}
+//   mainWindow.webContents.openDevTools()
+// }
+
+// app.whenReady().then(() => {
+//   autoUpdater.checkForUpdatesAndNotify();
+//   createWindow()
+
+//   app.on('activate', function () {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
+
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+// ipcMain.on('app_version', e => {
+//   e.sender.send('app_version', {version: app.getVersion()})
+// })
+
+// autoUpdater.on('checking-for-update', () => {
+//   mainWindow.webContents.send('check_update');
+// })
+
+// autoUpdater.on('update-not-available', () => {
+//   mainWindow.webContents.send('update_not_available');
+// })
+
+// autoUpdater.on('update-available', () => {
+//   mainWindow.webContents.send('update_available');
+// })
+
+// autoUpdater.on('update-downloaded', () => {
+//   mainWindow.webContents.send('update_download');
+// })
+
+// ipcMain.on('restart_app', () => {
+//   autoUpdater.quitAndInstall();
+// })
+
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+let win;
+
+const createWindow = () => {
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+
+  win.loadFile("index.html");
+};
 
 app.whenReady().then(() => {
-  autoUpdater.checkForUpdates();
-  createWindow()
+	createWindow();
+	
+  	// 업데이트 사항을 체크하고 알려주는 메서드
+	autoUpdater.checkForUpdatesAndNotify();
+});
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+ipcMain.on("app_version", (event) => {
+  	// app_version 채널로 현재 version을 보내는 코드
+	event.sender.send("app_version", { version: app.getVersion() });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+autoUpdater.on("update-available", () => {
+	win.webContents.send("update_available");
+});
 
-ipcMain.on('app_version', e => {
-  e.sender.send('app_version', {version: app.getVersion()})
-})
+autoUpdater.on("update-downloaded", () => {
+	win.webContents.send("update_downloaded");
+});
 
-autoUpdater.on('checking-for-update', () => {
-  console.log('check update')
-  mainWindow.webContents.send('check_update');
-})
-
-autoUpdater.on('update-not-available', () => {
-  mainWindow.webContents.send('update_not_available');
-})
-
-autoUpdater.on('update-available', () => {
-  console.log('update')
-  mainWindow.webContents.send('update_available');
-})
-
-autoUpdater.on('update-downloaded', () => {
-  console.log('download success')
-  mainWindow.webContents.send('update_download');
-})
-
-ipcMain.on('restart_app', () => {
-  console.log('restart')
-  autoUpdater.quitAndInstall();
-})
+ipcMain.on("restart_app", () => {
+  	// restart_app 채널로 수신 받았을 때
+  	// autoUpdater의 함수를 사용하여
+  	// 일렉트론 앱 종료 후 최신 버전으로 다시 설치
+	autoUpdater.quitAndInstall();
+});
--- renderer.js
+++ renderer.js
@@ -1,30 +1,73 @@
-const counter = document.getElementById('counter')
+// const counter = document.getElementById('counter')
 
-const version = document.getElementById('version');
-const message = document.getElementById('message');
-const restart = document.getElementById('restart');
+// const version = document.getElementById('version');
+// const message = document.getElementById('message');
+// const restart = document.getElementById('restart');
 
-window.electronAPI.checkVersion((event, value) => {
-    version.innerText = value.version
-})
+// window.electronAPI.checkVersion((event, value) => {
+//     version.innerText = value.version
+// })
 
-window.electronAPI.checkUpdate((event, value) => {
-    console.log('업데이트 버전 확인 중')
-})
+// window.electronAPI.checkUpdate((event, value) => {
+//     console.log('업데이트 버전 확인 중')
+// })
 
-window.electronAPI.activeUpdate((event, value) => {
-    console.log('업데이트 준비 완료')
-})
+// window.electronAPI.activeUpdate((event, value) => {
+//     console.log('업데이트 준비 완료')
+// })
 
-window.electronAPI.deactiveUpdate((event, value) => {
-    console.log('업데이트 불가')
-})
+// window.electronAPI.deactiveUpdate((event, value) => {
+//     console.log('업데이트 불가')
+// })
 
-window.electronAPI.downloadNewV((event, value) => {
-    message.innerText = '업데이트 파일 다운로드가 완료되었습니다. 재시작 시 업데이트가 반영됩니다.'
-})
+// window.electronAPI.downloadNewV((event, value) => {
+//     message.innerText = '업데이트 파일 다운로드가 완료되었습니다. 재시작 시 업데이트가 반영됩니다.'
+// })
 
-restart.addEventListener('click', () => {
-    console.log('click restart')
-    window.electronAPI.restart();
-})
+// restart.addEventListener('click', () => {
+//     console.log('click restart')
+//     window.electronAPI.restart();
+// })
+
+const version = document.querySelector("#version");
+
+ipcRenderer.send("app_version"); // app_version 채널로 송신
+// app_version 채널에서 송신
+ipcRenderer.on("app_version", (event, data) => { 
+  	// event 모두 종료 후 다음 코드 실행
+	ipcRenderer.removeAllListeners("app_version");
+  	// 업데이트가 필요한 경우 현재 버전을 알려주는 메시지입니다.
+	version.innerText = `현재 버전:  ${data.version}`;
+});
+
+const notification = document.querySelector(".notification");
+const message = document.querySelector(".update-message");
+const closeButton = document.querySelector("#close-button");
+const restartButton = document.querySelector("#restart-button");
+
+ipcRenderer.on("update_available", () => {
+	ipcRenderer.removeAllListeners("update_available");
+	message.innerText = "업데이트 파일을 다운로드 중입니다...";
+	notification.classList.remove("hidden");
+});
+
+// update_downloaded 채널로 송/수신
+ipcRenderer.on("update_downloaded", () => {
+	ipcRenderer.removeAllListeners("update_downloaded");
+  	// 업데이트 완료 후 해당 메시지를 보여줍니다.
+	message.innerText =
+		"업데이트 파일 다운로드를 마쳤습니다. \n 재시작을 하면 업데이트 버전이 실행됩니다. \n 재시작 하시겠습니까?";
+  	// 닫기 / 재시작 버튼을 렌더링합니다.
+	restartButton.classList.remove("hidden");
+	notification.classList.remove("hidden");
+});
+
+function closeNotification() {
+	notification.classList.add("hidden");
+}
+function restartApp() {
+	ipcRenderer.send("restart_app");
+}
+
+closeButton.addEventListener("click", closeNotification);
+restartButton.addEventListener("click", restartApp);
